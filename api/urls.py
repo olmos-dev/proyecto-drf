e7e96edddf7352fc7d9c81c9ec7454edafe5464c
Alberto Olmos <albrtho94@gmail.com>
2025-11-26 11:04:53 -0600

    La clase APIView

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
-from api.views import listar_peliculas, ver_pelicula, crear_pelicula, editar_pelicula,eliminar_pelicula
+from api.views import ListarPeliculasApiView, DetallePeliculaApiView
+
 
 urlpatterns = [
-    path('listar-peliculas', listar_peliculas, name="index.peliculas"),
-    path('crear-pelicula/', crear_pelicula, name="crear.pelicula"),
-    path('ver-pelicula/<int:pk>', ver_pelicula, name="ver.pelicula"),
-    path('editar-pelicula/<int:pk>', editar_pelicula, name="editar.pelicula"),
-    path('eliminar-pelicula/<int:pk>', eliminar_pelicula, name="eliminar.pelicula")
+    path('listar-peliculas', ListarPeliculasApiView.as_view(), name="listar.peliculas"),
+    path('detalle-pelicula/<int:pk>', DetallePeliculaApiView.as_view(), name="detalle.pelicula"),
 ]
--- api/views.py
+++ api/views.py
@@ -4,70 +4,54 @@ from rest_framework.decorators import api_view
 from peliculas.models import Pelicula
 from api.serializers import PeliculaSerializer
 from rest_framework import status
+from rest_framework.views import APIView
 
-@api_view(['GET'])
-def listar_peliculas(request):
-  """INDEX"""
-  if request.method == 'GET':
-      peliculas = Pelicula.objects.all()
-      serializador = PeliculaSerializer(peliculas, many=True)
-      return Response(serializador.data, status=status.HTTP_200_OK)
-  
 
-@api_view(["POST"])
-def crear_pelicula(request):
-  """CREATE"""
-  if request.method == "POST":
-      serializador = PeliculaSerializer(data = request.data)
-      if serializador.is_valid():
-        serializador.save()
+class ListarPeliculasApiView(APIView):
+    def get(self, request):
+        peliculas = Pelicula.objects.all()
+        serializador = PeliculaSerializer(peliculas, many=True)
         return Response(serializador.data, status=status.HTTP_200_OK)
-      return Response(serializador.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    def post(self, request):
+        serializador = PeliculaSerializer(data = request.data)
+        if serializador.is_valid():
+            serializador.save()
+            return Response(serializador.data, status=status.HTTP_200_OK)
+        return Response(serializador.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+class DetallePeliculaApiView(APIView):
+    def get(self, request, pk):
+        try:
+            pelicula = Pelicula.objects.get(pk=pk)
+        except Pelicula.DoesNotExist:
+            return Response({"mensaje": "Pelicula no encontrada"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializador = PeliculaSerializer(pelicula)
+        return Response(serializador.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        try:
+            pelicula = Pelicula.objects.get(pk=pk)
+        except Pelicula.DoesNotExist:
+            return Response({"mensaje": "Pelicula no encontrada"}, status=status.HTTP_404_NOT_FOUND)
         
-@api_view(["GET","PUT"])
-def editar_pelicula(request, pk):
-  """UPDATE"""
-  try:
-      pelicula = Pelicula.objects.get(pk=pk)
-  except Pelicula.DoesNotExist:
-      return Response({"mensaje": "Pelicula no encontrada"}, status=status.HTTP_404_NOT_FOUND)
+        serializador = PeliculaSerializer(pelicula, data=request.data)
+        if serializador.is_valid():
+            serializador.save()
+            return Response(serializador.data, status=status.HTTP_200_OK)
+        return Response(serializador.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        try:
+            pelicula = Pelicula.objects.get(pk=pk)
+        except Pelicula.DoesNotExist:
+            return Response({"mensaje": "Pelicula no encontrada"}, status=status.HTTP_404_NOT_FOUND)
+        pelicula.delete()
+        return Response({"mensaje": "Pelicula eliminada correctamente"}, status=status.HTTP_204_OK)
 
-  if request.method == 'GET':
-      serializador = PeliculaSerializer(pelicula)
-      return Response(serializador.data)
+            
 
-  elif request.method == 'PUT':
-      serializador = PeliculaSerializer(pelicula, data=request.data)
-      if serializador.is_valid():
-          serializador.save()
-          return Response(serializador.data, status=status.HTTP_200_OK)
-      return Response(serializador.errors, status=status.HTTP_400_BAD_REQUEST)
+   
+   
 
-@api_view(["GET", "DELETE"])
-def eliminar_pelicula(request, pk):
-  """DELETE"""
-  try:
-      pelicula = Pelicula.objects.get(pk=pk)
-  except Pelicula.DoesNotExist:
-      return Response({"mensaje": "Pelicula no encontrada"}, status=status.HTTP_404_NOT_FOUND)
-
-  if request.method == 'GET':
-      serializador = PeliculaSerializer(pelicula)
-      return Response(serializador.data)
-
-  elif request.method == 'DELETE':
-      pelicula.delete()
-      return Response({"mensaje": "Pelicula eliminada correctamente"}, status=status.HTTP_204_OK)
-        
-
-@api_view(["GET"])
-def ver_pelicula(request, pk):
-  """SHOW"""
-  try:
-      pelicula = Pelicula.objects.get(pk=pk)
-  except Pelicula.DoesNotExist:
-      return Response({"mensaje": "Pelicula no encontrada"}, status=status.HTTP_404_NOT_FOUND)
-
-  serializador = PeliculaSerializer(pelicula)
-  return Response(serializador.data, status=status.HTTP_200_OK)
-  
