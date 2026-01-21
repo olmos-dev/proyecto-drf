cf62fdca67f1340eab5bb1bc1441a501919da735
Alberto Olmos <albrtho94@gmail.com>
2026-01-21 14:01:24 -0600

    Registrar y agregar el token al momento de crear

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,12 +1,25 @@
 from rest_framework.decorators import api_view
 from user_app.api.serializers import RegistrationSerializer
 from rest_framework.response import Response 
+from rest_framework.authtoken.models import Token
+
 
 
 @api_view(['POST'])
 def registration_view(request):
   if request.method == 'POST':
     serializer = RegistrationSerializer(data=request.data)
+    data = {}
+
     if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
+      cuenta = serializer.save()
+      data['response'] = "Registro exitoso"
+      data['username'] = cuenta.username
+      data['email'] = cuenta.email
+      token, created = Token.objects.get_or_create(user=cuenta)
+      data["token"] = token.key
+
+    else:
+      data = serializer.errors
+
+    return Response(data)
