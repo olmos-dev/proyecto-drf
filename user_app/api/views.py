d9ef2d58564542734e3e0cfd60a465e28f9dcca2
Alberto Olmos <albrtho94@gmail.com>
2026-05-06 22:48:43 -0600

    Modulo de regitrar e implementar JWT autenticacion

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import AuthenticationFailed
+
 
 @api_view(['POST'])
 def logout_view(request):
@@ -22,8 +25,16 @@ def registration_view(request):
       data['response'] = "Registro exitoso"
       data['username'] = cuenta.username
       data['email'] = cuenta.email
-      token, created = Token.objects.get_or_create(user=cuenta)
-      data["token"] = token.key
+      
+      #token, created = Token.objects.get_or_create(user=cuenta)
+      #data["token"] = token.key
+
+      refresh = RefreshToken.for_user(cuenta) #se le pasa el user
+
+      data["token"] = {
+          'refresh': str(refresh),
+          'access': str(refresh.access_token),
+      }
 
     else:
       data = serializer.errors
