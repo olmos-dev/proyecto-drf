40bed1057b436a98f91cfe9c08b591d0b1d61755
Alberto Olmos <albrtho94@gmail.com>
2026-05-14 17:48:40 -0600

    Api testing - Registro

--- peliculas/tests.py
+++ peliculas/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+class TestRegistro(APITestCase):
+  
+  def test_registrar(self):
+    data = {
+      "username":"testcase",
+      "email":"testcase@mail.com",
+      "password":"Hondacr123",
+      "password2":"Hondacr123"
+    }
+    response = self.client.post(reverse("register"), data)
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    
