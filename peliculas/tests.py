12ede74e4f6b430e2097a93a6fb3e1f152ea5ed6
Alberto Olmos <albrtho94@gmail.com>
2026-05-15 17:40:44 -0600

    Prueba unitaria - Plataforma Strema

--- peliculas/tests.py
+++ peliculas/tests.py
@@ -1,43 +1,42 @@
+from django.test import TestCase
+
+# Create your tests here.
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
+from api.serializers import serializers
+from peliculas import models
 
-class TestRegistro(APITestCase):
-  
-  def test_registrar(self):
-    data = {
-      "username":"testcase",
-      "email":"testcase@mail.com",
-      "password":"Hondacr123",
-      "password2":"Hondacr123"
-    }
-    response = self.client.post(reverse("register"), data)
-    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-class TestAutenticacion(APITestCase):
-
+class PlataformaStreamTestCase(APITestCase):
   def setUp(self):
     self.user = User.objects.create_user(
-      username="alberto",
+      username="olmos",
       password="Hondacr123"
     )
-
-  def test_login(self):
-    data = {
-      "username":"alberto",
-      "password":"Hondacr123"
-    }
-    response = self.client.post(reverse('login'), data)
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-  def test_logout(self):
-    self.token = Token.objects.get(user__username="alberto")
+    self.token = Token.objects.get(user__username=self.user)
     self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-    response = self.client.post(reverse('logout'))
+    self.stream = models.Plataforma.objects.create(
+      nombre = "Netflix",
+      acerca = "Lorem ipsum",
+      sitio_web = "https//neflix.com"
+    )
+
+  def test_plataforma_stream_crear(self):
+    data = {
+      "nombre":"netflix",
+      "acerca":"Lorem ipsum",
+      "sitio_web": "https//neflix.com"
+    }
+    response = self.client.post(reverse("listar.plataforma"), data)
+    self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+  def test_listar_plataforma_stream(self):
+    response = self.client.get(reverse("listar.plataforma"))
     self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-
-    
+  def test_individual_plataforma_stream(self):
+    response = self.client.get(reverse('detalle.plataforma', args=(self.stream.id,)))
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+
+class TestRegistro(APITestCase):
+  
+  def test_registrar(self):
+    data = {
+      "username":"testcase",
+      "email":"testcase@mail.com",
+      "password":"Hondacr123",
+      "password2":"Hondacr123"
+    }
+    response = self.client.post(reverse("register"), data)
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class TestAutenticacion(APITestCase):
+
+  def setUp(self):
+    self.user = User.objects.create_user(
+      username="alberto",
+      password="Hondacr123"
+    )
+
+  def test_login(self):
+    data = {
+      "username":"alberto",
+      "password":"Hondacr123"
+    }
+    response = self.client.post(reverse('login'), data)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_logout(self):
+    self.token = Token.objects.get(user__username="alberto")
+    self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+    response = self.client.post(reverse('logout'))
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    
