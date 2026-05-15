17165ac8cb213b738a5e0cf19b0e91ae9b9543e6
Alberto Olmos <albrtho94@gmail.com>
2026-05-15 10:41:50 -0600

    Pruebas unitarias - Autenticacion - Login & Logout

--- peliculas/tests.py
+++ peliculas/tests.py
@@ -17,4 +17,27 @@ class TestRegistro(APITestCase):
     response = self.client.post(reverse("register"), data)
     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+class TestAutenticacion(APITestCase):
+
+  def setUp(self):
+    self.user = User.objects.create_user(
+      username="alberto",
+      password="Hondacr123"
+    )
+
+  def test_login(self):
+    data = {
+      "username":"alberto",
+      "password":"Hondacr123"
+    }
+    response = self.client.post(reverse('login'), data)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_logout(self):
+    self.token = Token.objects.get(user__username="alberto")
+    self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+    response = self.client.post(reverse('logout'))
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
     
